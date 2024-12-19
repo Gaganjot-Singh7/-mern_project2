efd64b854e515bd50726ec7d0d2df8e2d0117e59
gaganjot <jot77430@gmail.com>
2024-12-20 04:23:56 +0530

    Update(UI)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,20 +26,19 @@ function Navbar() {
         <>
 
             <div className='nav'>
-                <NavLink to="/" className='nav_logo'>GAGANJOT SINGH</NavLink>
+                <NavLink to="/" className='nav_logo'>TECHNOLEX</NavLink>
                 <ul className='nav_iteams'>
-                    <li ><NavLink to="/home" className='nav_list'>Home</NavLink></li>
-                    <li ><NavLink to="/about" className='nav_list'>About</NavLink></li>
-                    <li ><NavLink to="/contact" className='nav_list'>Contact</NavLink></li>
-                    <li ><NavLink to="/services" className='nav_list'>Services</NavLink></li>
+                    <li ><NavLink to="home" className='nav_list'>Home</NavLink></li>
+                    <li ><NavLink to="about" className='nav_list'>About</NavLink></li>
+                    <li ><NavLink to="contact" className='nav_list'>Contact</NavLink></li>
+                    <li ><NavLink to="services" className='nav_list'>Services</NavLink></li>
 
-                    {isLogin ? <li><NavLink to="/Logout" className='nav_list'>Logout</NavLink></li> :
+                    {isLogin ? <li><NavLink to="Logout" className='nav_list'>Logout</NavLink></li> :
                         <>
-                            <li ><NavLink to="/login" className='nav_list'>Login</NavLink></li>
-                            <li ><NavLink to="/register" className='nav_list'>Register</NavLink></li>
+                            <li ><NavLink to="login" className='nav_list'>Login</NavLink></li>
+                            <li ><NavLink to="register" className='nav_list'>Register</NavLink></li>
                         </>
                     }
-                    <li><NavLink to="/admin">Admin</NavLink></li>
 
 
 
--- server/db/conn.js
+++ server/db/conn.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 
 const Db=async()=>{
    try {
-    await mongoose.connect("mongodb+srv://jot77430:gaganjot03082002@cluster0.l1o5djw.mongodb.net/mern")
+    await mongoose.connect("mongodb+srv://jot77430:Sales01%402021@cluster0.l1o5djw.mongodb.net/mern?retryWrites=true&w=majority&appName=Cluster0")
    } catch (error) {
-    console.error("Database connection error");
+    console.error("Database connection error",error);
    }
 }
 
